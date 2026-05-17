739846bf91df4ac89a0775958981572253aa9143
Miguel Ferreira <estudanteavancado3@gmail.com>
2026-05-17 20:27:40 +0100

    Reajustando a rota do proff

--- backend/routes/professorRoutes.js
+++ backend/routes/professorRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { getDisciplinas } = require('../controllers/professorController');
 const { authMiddleware } = require('../middleware/authMiddleware');
 
+// Apenas professores autenticados podem aceder
 router.get('/disciplinas', authMiddleware, getDisciplinas);
 
 module.exports = router;
